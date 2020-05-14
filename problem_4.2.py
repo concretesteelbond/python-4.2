c4e51d3422e50591b1b0fe6d57b31559eb3e3565
Mahesh Gaikwad <39650869+concretesteelbond@users.noreply.github.com>
2020-05-14 19:47:13 +0530

    Update problem_4.2.py

--- problem_4.2.py
+++ problem_4.2.py
@@ -3,9 +3,9 @@ ran_list = []
 random.seed(150)
 for i in range(0,25):
     ran_list.append(round(100*random.random(),1))
+import statistics
 
-	def problem4_2(ran_list):
+def problem4_2(ran_list):
     """ Compute the mean and standard deviation of a list of floats """
-    import statistics
-	print("Mean: ", statistics.mean(ran_list))
+    print("Mean: ", statistics.mean(ran_list))
     print("Standard deviation: ", statistics.stdev(ran_list))
