c221d934bd033f05769b0d7a3047a1fee72561e0
Mahesh Gaikwad <39650869+concretesteelbond@users.noreply.github.com>
2020-05-14 19:36:05 +0530

    Create problem_4.2.py

---
+++ problem_4.2.py
@@ -0,0 +1,7 @@
+import statistics
+def problem4_2(ran_list):
+    """ Compute the mean and standard deviation of a list of floats """
+    print(statistics.mean(ran_list))
+    print(statistics.stdev(ran_list))
+
+problem4_2([1,2,3,4,5])
